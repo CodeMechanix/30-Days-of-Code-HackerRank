5c833169306270d7f1a9c4d657a176b72d729b31
Hasan Mahmud <hasan.357cse@gmail.com>
2017-03-31 10:07:37 +0600

    Update Day 4 Class vs. Instance.cpp

--- Day 4 Class vs. Instance.cpp
+++ Day 4 Class vs. Instance.cpp
@@ -40,3 +40,22 @@ void Person::yearPasses()
 {
     age += 1;
 }
+int main()
+{
+    int t;
+	int age;
+    cin >> t;
+    for(int i=0; i < t; i++)
+    {
+    	cin >> age;
+        Person p(age);
+        p.amIOld();
+        for(int j=0; j < 3; j++)
+        {
+        	p.yearPasses(); 
+        }
+        p.amIOld();
+		cout << '\n';
+    }
+    return 0;
+}
