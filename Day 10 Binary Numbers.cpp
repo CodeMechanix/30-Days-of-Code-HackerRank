11fba04db8d6c24999313cbe15a0bac540a76a5d
Hasan Mahmud <hasan404cse@gmail.com>
2017-04-22 14:39:39 +0600

    Add files via upload

---
+++ Day 10 Binary Numbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int bits = sizeof(n)*8;
+    int consecutiveOnes, consecutiveOnesMax;
+    consecutiveOnes = consecutiveOnesMax = 0;
+    while (bits--)
+    {
+        if (n & 1)
+        {
+            ++consecutiveOnes;
+        }
+        else
+        {
+            if (consecutiveOnes > consecutiveOnesMax)
+            {
+                consecutiveOnesMax = consecutiveOnes;
+            }
+            consecutiveOnes = 0;
+        }
+        n >>= 1;
+    }
+    cout << consecutiveOnesMax << endl;
+    return 0;
+}
